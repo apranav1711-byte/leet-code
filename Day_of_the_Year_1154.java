dc5e5de776b9684db3de63d2c2219e69b79c4678
monkey <apranav1711@gmail.com>
2026-02-17 23:42:43 +0530

    saved

---
+++ Day_of_the_Year_1154.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int dayOfYear(String date) {
+        int[] months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        int year = Integer.parseInt(date.substring(0, 4));
+        int n = ((date.charAt(5) - '0') * 10 + (date.charAt(6) - '0')) - 1;
+        int p = ((date.charAt(8) - '0') * 10 + (date.charAt(9) - '0'));
+
+        for (int i = 0; i < n; i++) {
+            p = p + months[i];
+        }
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            if (n >= 2) {
+                p += 1;
+            }
+        }
+
+        return p;
+
+    }
+}
---
+++ X_of_a_Kind_in_a_Deck_of_Cards_914.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+class X_of_a_Kind_in_a_Deck_of_Cards_914 {
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int card : deck) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        int gcd = 0;
+
+        for (int freq : map.values()) {
+            gcd = findGCD(gcd, freq);
+        }
+
+        if (gcd > 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private int findGCD(int a, int b) {
+        if (a == 0)
+            return b;
+        return findGCD(b % a, a);
+    }
+
+}
