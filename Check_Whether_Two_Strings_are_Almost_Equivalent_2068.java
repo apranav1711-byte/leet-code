0fe0c7f2393ef9698204d03aa25e913c1132fe7a
Pranav Aggarwal <Apranav1711@gmail.com>
2026-04-08 21:26:34 +0530

    2068

---
+++ Check_Whether_Two_Strings_are_Almost_Equivalent_2068.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < word1.length(); i++){
+            map.put(word1.charAt(i), map.getOrDefault(word1.charAt(i), 0) + 1);
+            map.put(word2.charAt(i), map.getOrDefault(word2.charAt(i), 0) - 1);
+        }
+
+        for(int val : map.values()){
+            if(Math.abs(val) > 3){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
